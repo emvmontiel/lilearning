36e081f7e56fb75bebed66ed40ae00442fed6f79
emvmontiel <emvmontiel@gmail.com>
2026-02-04 17:39:29 +0800

    access data using dot notation

--- Ex_Files_Intro_to_Web_APIs/Exercise Files/04_04_GiphyAccess/begin/script.js
+++ Ex_Files_Intro_to_Web_APIs/Exercise Files/04_04_GiphyAccess/begin/script.js
@@ -1,7 +1,12 @@
-fetch('https://api.giphy.com/v1/gifs/random?api_key=bwBAcjO5j2Cec47TG7flBCn7Gdw57Fo8&tag=squirrel&rating=g')
+fetch('https://api.giphy.com/v1/gifs/random?api_key=1CVUPKgBTkGqkAxC09x1mpej5kDtTw4O&tag=screaming&rating=g')
 .then(function(response) {
 	return response.json();
 })
+// .then(function(jsonData) {
+// 	console.log(jsonData);
+// })
 .then(function(jsonData) {
-	console.log(jsonData);
+	console.log(jsonData)
+	var gifUrl = jsonData.data.images.original.url; // Dot notation
+	console.log(gifUrl);
 })
