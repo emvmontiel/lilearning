fb3a1f9bb009577b8f317a8fa60843eb338ffe15
emvmontiel <emvmontiel@gmail.com>
2026-02-05 13:46:29 +0800

    fixed format; added comments

--- Ex_Files_Intro_to_Web_APIs/Exercise Files/04_05_GiphyDataHtml/begin/script.js
+++ Ex_Files_Intro_to_Web_APIs/Exercise Files/04_05_GiphyDataHtml/begin/script.js
@@ -1,14 +1,21 @@
-fetch('https://api.giphy.com/v1/gifs/random?api_key=bwBAcjO5j2Cec47TG7flBCn7Gdw57Fo8&tag=squirrel&rating=g')
-.then(function(response) {
-	return response.json();
-})
-.then(function(jsonData) {
-	console.log(jsonData);
-	var gifUrl = jsonData.data.images.original.url
-	console.log(gifUrl)
-	var gifImg = document.createElement("img")
-	gifImg.setAttribute("src", gifUrl)
-	document.body.appendChild(gifImg)
-})
+fetch('https://api.giphy.com/v1/gifs/random?api_key=1CVUPKgBTkGqkAxC09x1mpej5kDtTw4O&tag=screaming')
+.then(
+	function(response) {
+		return response.json();
+}
+)
+.then(
+	function(jsonData) {
+		console.log(jsonData);
+		
+		var gifUrl = jsonData.data.images.original.url;
+		console.log(gifUrl);
+		
+		var gifImg = document.createElement("img"); // Creates an image element
+		gifImg.setAttribute("src", gifUrl); // Sets the source attribute for the image
+		document.body.appendChild(gifImg); // Adds the gif onto the page
+	}
+)
 
 // Research and Understand here: https://gemini.google.com/share/f690eca59a6d
+// Your API Key: 1CVUPKgBTkGqkAxC09x1mpej5kDtTw4O
