352a2ba75ed94a585265d04641946e074d88f14f
emvmontiel <emvmontiel@gmail.com>
2026-02-05 14:11:22 +0800

    solved

--- Ex_Files_Intro_to_Web_APIs/Exercise Files/04_06_GiphyChallenge/script.js
+++ Ex_Files_Intro_to_Web_APIs/Exercise Files/04_06_GiphyChallenge/script.js
@@ -1,14 +1,24 @@
-fetch('https://api.giphy.com/v1/gifs/random?api_key=bwBAcjO5j2Cec47TG7flBCn7Gdw57Fo8&tag=squirrel&rating=g')
+fetch('https://api.giphy.com/v1/gifs/random?api_key=1CVUPKgBTkGqkAxC09x1mpej5kDtTw4O&tag=scream+love&rating=g')
 .then(function(response) {
 	return response.json();
 })
 .then(function(jsonData) {
 	console.log(jsonData);
-	var gifUrl = jsonData.data.images.original.url
+	var gifUrl = jsonData.data.images.original.url // Retrieves gif URL and puts it into gifUrl variable
 	console.log(gifUrl)
 
-	//Create gif on page
+	// Create gif on page
 	var gif = document.createElement('img');
 	gif.setAttribute('src', gifUrl);
 	document.body.appendChild(gif)
+	
+	var gifTitle = jsonData.data.title; // Retrieves gif title and puts it into gifTitle variable
+
+	var title = document.createElement('h1'); // Creates h1 element
+	title.innerText = gifTitle; // Adds gifTitle to HTML
+	document.body.appendChild(title);
+ 
+
 })
+
+// Your API Key: 1CVUPKgBTkGqkAxC09x1mpej5kDtTw4O
